50d4e9b8553856f8f542676aa46e230618d17616
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2018-10-10 15:53:52 -0300

    ajustando formatação conforme o pep8

--- media.py
+++ media.py
@@ -1,13 +1,16 @@
 import webbrowser
 
+
 class Video():
     def __init__(self, title):
         self.title = title
 
+
 class Movie(Video):
     VALID_RATINGS = ["P", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         Video.__init__(self, movie_title)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
--- my_movies.py
+++ my_movies.py
@@ -3,22 +3,29 @@ import fresh_tomatoes
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/pt/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/pt/b/b0/" +
+                     "Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 star_wars = media.Movie("Star Wars",
                         "The Force Awakens",
-                        "https://upload.wikimedia.org/wikipedia/pt/a/ae/Starwars_06.jpg",
+                        "https://upload.wikimedia.org/wikipedia/pt/a/ae/" +
+                        "Starwars_06.jpg",
                         "https://www.youtube.com/watch?v=4r0287tUEgk")
 
 captain_marvel = media.Movie("Captain Marvel",
-                             "Is an upcoming American superhero film based on the Marvel Comics.",
-                             "https://upload.wikimedia.org/wikipedia/en/8/85/Captain_Marvel_poster.jpg",
+                             "Is an upcoming American superhero film based " +
+                             "on the Marvel Comics.",
+                             "https://upload.wikimedia.org/wikipedia/en/8/" +
+                             "85/Captain_Marvel_poster.jpg",
                              "https://www.youtube.com/watch?v=Z1BCujX3pw8")
 
 venom = media.Movie("Venom",
-                    " In Venom, journalist Brock gains superpowers after being bound to an alien symbiote whose species plans to invade Earth",
-                    "http://t2.gstatic.com/images?q=tbn:ANd9GcRvT1RlWWfdCEOyeF00_yBwYzSg4_Nm9oWPNfRMJGviWb6hnLLW",
+                    " In Venom, journalist Brock gains superpowers after " +
+                    "being bound to an alien symbiote whose species plans " +
+                    "to invade Earth",
+                    "http://t2.gstatic.com/images?q=tbn:ANd9GcRvT1RlWWfdC" +
+                    "EOyeF00_yBwYzSg4_Nm9oWPNfRMJGviWb6hnLLW",
                     "https://www.youtube.com/watch?v=sVjQw8M_FSM")
 
 movies = [avatar, star_wars, captain_marvel, venom]
